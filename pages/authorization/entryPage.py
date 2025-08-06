da68beed8290f7d1bc5b2a0e77542f0db6e9c064
tendomuse <55035239+tendo-muse@users.noreply.github.com>
2025-08-07 01:55:31 +0300

    Обновил код
    1. Добавил комментарии
    2. Убрал @property
    3. Уточнил exception для исключений
    4. Добавил класс со степами
    5. Изменил код автотестов под логику степов
    6. Добавил allure

--- pages/authorization/entryPage.py
+++ pages/authorization/entryPage.py
@@ -4,17 +4,17 @@ from pages.authorization.baseAuthorizationPage import BaseAuthorizationPage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
+# Модель страницы входа
 class EntryPage(BaseAuthorizationPage):
     def __init__(self, browser):
         super().__init__(browser)
 
-    @property
     def window_is_displayed(self):
         try:
             element = WebDriverWait(self.browser, 10).until(
                 EC.visibility_of_element_located((By.XPATH, '//div[@data-qa="applicant-login-card"]')))
             return element.is_displayed()
-        except TimeoutException:
+        except NoSuchElementException:
             return False
 
     def entry_button_click(self):
